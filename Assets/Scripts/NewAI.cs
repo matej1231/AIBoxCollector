2542e3f789d470c0625ac12abbf6ba0332086521
Matej Perak <38085674+matej1231@users.noreply.github.com>
2021-03-26 18:51:03 +0100

    Complete project

---
+++ Assets/Scripts/NewAI.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class NewAI : MonoBehaviour
+{
+    private Queue<GameObject> newQueue = new Queue<GameObject>();
+    private GameObject[] allBlocks;
+    public Rigidbody2D rb;
+    public GameObject redBasket, blueBasket;
+    public Transform positionForHolding;
+    //public Button button;
+    
+    //int count = -1;
+    void Update()
+    {
+        Debug.Log(newQueue.Count);
+
+        newQueue = queueFunc();
+        GoToFirstBlock();
+    }
+
+    public Queue<GameObject> queueFunc()
+    {
+        /*if(count != allBlocks.Length)
+        {
+            allBlocks = GameObject.FindGameObjectsWithTag("clone");
+            count = allBlocks.Length;
+        }*/
+
+        //button.onClick.AddListener( () => FindClones() );
+
+        //FIX IT - CHECKING ALL THE TIME
+        allBlocks = GameObject.FindGameObjectsWithTag("clone");
+        newQueue = new Queue<GameObject>();
+
+        foreach (GameObject go in allBlocks)
+        {
+            Debug.Log("Now in Queue: " + newQueue.Count);
+            newQueue.Enqueue(go);
+        }
+        return newQueue;
+    }
+
+    /*public void FindClones()
+    {
+        allBlocks = GameObject.FindGameObjectsWithTag("clone");
+    }*/
+
+
+    public void GoToFirstBlock()
+    {
+        if (newQueue.Count >= 1)
+        {
+            GameObject target = newQueue.Dequeue();
+            target.layer = 9; //Can be fixed, but not too important
+
+            if (positionForHolding.childCount <= 0)
+            {
+                rb.MovePosition(new Vector2(transform.position.x, 0) + new Vector2(target.transform.position.x - transform.position.x, 0) * Time.fixedDeltaTime);
+            }
+
+            if (rb.IsTouching(target.gameObject.GetComponent<Collider2D>()))
+            {
+                target.transform.parent = positionForHolding;
+                target.transform.position = positionForHolding.position;
+                goToStash(target);
+            }
+        }
+    }
+
+    void goToStash(GameObject targetObj)
+    {
+        if (targetObj.name == "RedBlock(Clone)")
+        {
+            rb.MovePosition(new Vector2(transform.position.x, 0) + (new Vector2(redBasket.transform.position.x, 0) * 1f * Time.fixedDeltaTime));
+            if (rb.IsTouching(redBasket.gameObject.GetComponent<Collider2D>()))
+            {
+                Debug.Log("Spremam " + targetObj.name);
+                Destroy(targetObj);
+                //count--;
+            }
+        }
+        else if (targetObj.name == "BlueBlock(Clone)")
+        {
+            rb.MovePosition(new Vector2(transform.position.x, 0) + (new Vector2(blueBasket.transform.position.x, 0) * 1f * Time.fixedDeltaTime));
+            if (rb.IsTouching(blueBasket.gameObject.GetComponent<Collider2D>()))
+            {
+                Debug.Log("Spremam " + targetObj.name);
+                Destroy(targetObj);
+                //count--;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpawnBlocks.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBlocks : MonoBehaviour
+{
+    //Blocks
+    public GameObject redBlock, blueBlock;
+    private GameObject red, blue;
+
+    public void Spawn()
+    {
+        Vector3 redPosition = new Vector3(Random.Range(-4, 1), Random.Range(4, 7), 0);
+        Vector3 bluePosition = new Vector3(Random.Range(1, 4), Random.Range(4, 7), 0);
+
+        red = (GameObject)Instantiate(redBlock, redPosition, Quaternion.identity);
+        blue = (GameObject)Instantiate(blueBlock, bluePosition, Quaternion.identity);
+    }
+
+    /*public void Update()
+    {
+        Destroy(red, 2f);
+        Destroy(blue, 2f);
+    }*/
+}
